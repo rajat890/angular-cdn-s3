026626cbedba77edf0ba31bee8e87f9624878d19
rajat890 <59624003+rajat890@users.noreply.github.com>
2021-06-24 11:10:12 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'CDN wesbite with s3 on angular with latest changes on site';
+  title = 'CDN wesbite with s3 !!!!!!!!!!!!!!!!!!!';
 }
